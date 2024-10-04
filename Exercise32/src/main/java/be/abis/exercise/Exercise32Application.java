8a571c7e92280c6ba0e44ea88c8734a9ba3351bc
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2024-10-04 14:02:03 +0200

    association mapping

---
+++ Exercise32/src/main/java/be/abis/exercise/Exercise32Application.java
@@ -0,0 +1,13 @@
+package be.abis.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise32Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise32Application.class, args);
+    }
+
+}
---
+++ Exercise33/src/main/java/be/abis/exercise/converter/CancelConverter.java
@@ -0,0 +1,28 @@
+package be.abis.exercise.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+public class CancelConverter implements AttributeConverter<Boolean, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Boolean aBoolean) {
+        System.out.println("converting boolean to DB");
+        String valInDb = null;
+        if (aBoolean){
+            valInDb="c";
+        }
+        return valInDb;
+    }
+
+    @Override
+    public Boolean convertToEntityAttribute(String s) {
+        System.out.println("converting from DB to boolean");
+        Boolean valInJava=true;
+        if (s==null){
+            valInJava=false;
+        }
+        return valInJava;
+    }
+}
